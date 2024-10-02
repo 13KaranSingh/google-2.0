aec486e7f771bff345fbc8103df42530f6d4d162
Karanvir <kasingh@udel.edu>
2024-10-02 01:33:36 -0400

    set up done of tailwind css

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// this will be a simple functional component
+//rafce creates a react arrow function with a normal export
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-4xl font-bold text-blue-600">App</h1>
+    </div>
+  )
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import './global.css';
 
 // Get the root DOM element where the React app will be mounted
 const container = document.getElementById('root');
